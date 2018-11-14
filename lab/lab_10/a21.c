13a0700b4ebd3882576b4a6edd0417db6f409031
anton2920 <anton2920@gmail.com>
2018-11-14 22:22:27 +0300

    PC: adding programs

---
+++ lab/lab_10/a21.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+/* Logo (loop version) */
+main() {
+
+    /* Initializing variables */
+    int i, j;
+
+    /* Main part */
+    for (i = 0; i < 18; ++i) {
+        for (j = 0; j < 46; ++j) {
+            if (j == 0) {
+                printf("[");
+            } else if (j == 45) {
+                printf("]\n");
+            } else if ((!i || i == 17) && (j >= 1 && j < 15)) {
+                printf("[");
+            } else if ((i != 0 && i != 17) && (j < 6 || j > 39)) {
+                printf(":");
+            } else if ((i >= 4 && i <= 13) && j == 6) {
+                printf("[");
+            } else if (((i >= 1 && i <= 2) || (i >= 15 && i <= 16)) && ((j >= 6 && j <= 13) || (j > 31 && j <= 39))) {
+                printf(":");
+            } else if ((i >= 4 && i <= 13) && j == 39) {
+                printf("]");
+            } else if ((i == 3 || i == 14) && (j == 6 || j == 39)) {
+                printf(":");
+            } else if ((i == 3 || i == 14) && (j > 6 && j <= 13)) {
+                printf("[");
+            } else if ((i == 3 || i == 14) && (j > 31 && j < 39)) {
+                printf("]");
+            } else if (((i >= 1 && i <= 3) || (i >= 14 && i <= 16)) && (j == 14 || j == 31)) {
+                printf(":");
+            } else if ((!i || i == 17) && (j >= 31 && j <= 44)) {
+                printf("]");
+            } else if (i == 7 && j == 21) {
+                printf("BSTU");
+                j += 3;
+            } else if (i == 8 && j == 20) {
+                printf("18-SWE");
+                j += 5;
+            } else if (i == 9 && j == 10) {
+                printf("Pavlovsky Anton Evgenevich");
+                j += 25;
+            } else {
+                printf(" ");
+            }
+        }
+    }
+}
