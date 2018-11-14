f3e993ee0d712c4a1efea24206754f1e2fb72548
anton2920 <anton2920@gmail.com>
2018-11-14 22:18:53 +0300

    PC: 10.2 done in a tricky way

--- lab/lab_dev/main.c
+++ lab/lab_dev/main.c
@@ -1,49 +1,34 @@
 #include <stdio.h>
 
-/* Function declaration */
-long int fact(long int n);
-
-/* Sum of number sequence */
+/* Logo (loop version) */
 main() {
 
     /* Initializing variables */
-    float sum, e, x2, slog;
-    long int i, x, fac;
-    sum = 0;
-
-    /* I/O flow */
-    printf("Type X: ");
-    scanf("%ld", &x);
-    printf("Type accuracy: ");
-    scanf("%f", &e);
-
+    int i, j;
 
     /* Main part */
-    x2 = x * x;
-    fac = fact(x);
-    do {
-        slog = fac * x2;
-        x2 *= x * x;
-        sum += slog;
-    } while (slog >= e);
-
-    /* Final output */
-    printf("Sum = %f\n", sum);
-}
-
-long int fact(long int n) {
-
-    /* Initializing variables */
-    long int fact;
-    int counter;
-    fact = 1;
-
-    /* Main part */
-    for (counter = 1; counter <= n; ++counter) {
-        fact = fact * counter;
+    for (i = 0; i < 18; ++i) {
+        for (j = 0; j < 46; ++j) {
+            if ((j == 0) || ((!i || i == 17) && (j >= 1 && j < 15)) || ((i >= 4 && i <= 13) && j == 6) || ((i == 3 || i == 14) && (j > 6 && j <= 13))) {
+                printf("[");
+            } else if (j == 45) {
+                printf("]\n");
+            } else if (((i != 0 && i != 17) && (j < 6 || j > 39)) || (((i >= 1 && i <= 2) || (i >= 15 && i <= 16)) && ((j >= 6 && j <= 13) || (j > 31 && j <= 39))) || ((i == 3 || i == 14) && (j == 6 || j == 39)) || (((i >= 1 && i <= 3) || (i >= 14 && i <= 16)) && (j == 14 || j == 31))) {
+                printf(":");
+            } else if (((i >= 4 && i <= 13) && j == 39) || ((i == 3 || i == 14) && (j > 31 && j < 39)) || ((!i || i == 17) && (j >= 31 && j <= 44))) {
+                printf("]");
+            } else if (i == 7 && j == 21) {
+                printf("BSTU");
+                j += 3;
+            } else if (i == 8 && j == 20) {
+                printf("18-SWE");
+                j += 5;
+            } else if (i == 9 && j == 10) {
+                printf("Pavlovsky Anton Evgenevich");
+                j += 25;
+            } else {
+                printf(" ");
+            }
+        }
     }
-
-    /* Returning value */
-    return fact;
-
 }
