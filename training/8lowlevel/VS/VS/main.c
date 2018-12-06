fd07051ea8a6a4902b2d7838cdec7ba2b6136d31
anton2920 <anton2920@gmail.com>
2018-12-06 16:17:28 +0300

    ASUS: lgets()

---
+++ training/8lowlevel/VS/VS/main.c
@@ -0,0 +1,17 @@
+#include "gets.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+main() {
+
+	int n;
+	char buf[SIZE_BUF_GETS];
+	
+	printf("Type your string: ");
+	n = lgets(buf);
+	/* write(1, "Your string: ", 13);
+	* write(1, buf, n);
+	*/
+	printf("Your string: %s\n", buf);
+	system("pause");
+}
