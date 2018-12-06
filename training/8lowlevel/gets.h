b3fe1fdd1b26053296796b34ae3ecf445d3e20d2
anton2920 <anton2920@gmail.com>
2018-12-06 15:41:13 +0300

    PC: lgets()

--- training/8lowlevel/gets.h
+++ training/8lowlevel/gets.h
@@ -1,9 +1,13 @@
 #ifndef HEADER_FILE_GETS
 #define HEADER_FILE_GETS
 
+#if SYSTEM == LINUX
 #include <zconf.h>
+#elif SYSTEM == WINDOWS
+#include <Windows.h>
+#endif
 #define SIZE_BUF_GETS 1024
 
-int gets(char *);
+int lgets(char *);
 
 #endif
