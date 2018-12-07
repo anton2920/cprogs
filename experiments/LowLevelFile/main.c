612c4f5206cf236c641b6ff158de9e2f93c9df4d
anton <anton2920@gmail.com>
2018-12-07 13:24:04 +0300

    DELL: Low-level (small changes)

--- experiments/LowLevelFile/main.c
+++ experiments/LowLevelFile/main.c
@@ -9,11 +9,11 @@ main() {
 
     /* I/O flow */
     write_str_scr(">> Test program for low-level I/O <<\n", sizeof(">> Test program for low-level I/O <<\n") - 1);
-    linput("Type int: ", sizeof("Type int: ") - 1, &var);
+    input("Type int: ", sizeof("Type int: ") - 1, &var);
     write_str_scr("Your int: ", sizeof("Your int: ") - 1);
     write_int_scr(&var);
     write_str_scr("\n----------------------------------------------------------------\n", 66);
-    linput("Type the number of elements: ", sizeof("Type the number of elements: ") - 1, &n);
+    input("Type the number of elements: ", sizeof("Type the number of elements: ") - 1, &n);
     read_mas_key(mas, &n);
     write_mas_scr(mas, &n);
 }
