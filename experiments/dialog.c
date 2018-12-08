c96e78ada55116ed4f0c8e3c62a89a6020d82bd5
anton <anton2920@gmail.com>
2018-12-09 00:06:29 +0300

    DELL: project

--- experiments/dialog.c
+++ experiments/dialog.c
@@ -1,4 +1,5 @@
 #include <dialog.h>
+#include <curses.h>
 
 int main(void)
 {
---
+++ home/Project/cmake-build-debug/CMakeFiles/Project.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project.dir/main.c.o"
+  "CMakeFiles/Project.dir/libs/menu.c.o"
+  "Project.pdb"
+  "Project"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
