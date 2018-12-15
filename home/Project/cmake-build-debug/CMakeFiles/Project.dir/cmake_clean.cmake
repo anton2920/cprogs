bf8feaf4dfe2e3f5a35583cebc69d2eadfac1c1d
anton2920 <anton2920@gmail.com>
2018-12-15 23:44:54 +0300

    PC: small changes

--- home/Project/cmake-build-debug/CMakeFiles/Project.dir/cmake_clean.cmake
+++ home/Project/cmake-build-debug/CMakeFiles/Project.dir/cmake_clean.cmake
@@ -1,11 +1,5 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Project.dir/main.c.o"
-  "CMakeFiles/Project.dir/libs/menu.c.o"
-  "Project.pdb"
-  "Project"
-)
 
 # Per-language clean rules from dependency scanning.
-foreach(lang C)
+foreach(lang )
   include(CMakeFiles/Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
