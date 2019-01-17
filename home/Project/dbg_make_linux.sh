fcf29841da350340038e396d7d08355f4bb65723
anton <anton2920@gmail.com>
2019-01-17 04:53:51 +0300

    DELL: dbg_make_linux.sh

---
+++ home/Project/dbg_make_linux.sh
@@ -0,0 +1,1 @@
+gcc -std=c90 -Wall -c libs/menu.c && gcc -std=c90 -Wall -c libs/menu2.c && gcc -std=c90 -Wall -c libs/menu3.c && gcc -std=c90 -Wall -c main.c && gcc -std=c90 -Wall -c libs/math.c && gcc -std=c90 -Wall -c libs/tree2.c && gcc -std=c90 -Wall main.o menu.o menu2.o menu3.o math.o tree2.o -o dbg_main -ldialog -lncursesw -lm
