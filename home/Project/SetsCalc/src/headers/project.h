77a545d501bef6782fc8f989240707f94d819c8e
anton2920 <anton2920@gmail.com>
2019-02-11 21:47:39 +0300

    PC: Project: automake && autoconf

--- home/Project/SetsCalc/src/headers/project.h
+++ home/Project/SetsCalc/src/headers/project.h
@@ -30,7 +30,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <time.h>
-/* #include "../../config.h" */
+#include "../../config.h"
 
 /* main.c */
 void lclear(void); /* Clears console after completing */
