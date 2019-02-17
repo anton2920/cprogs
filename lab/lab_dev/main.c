036fec182d4b60e51e93bc4ae685eb249d95f3b8
anton <anton2920@gmail.com>
2019-02-17 22:23:10 +0300

    DELL: small changes on lab_19

--- lab/lab_dev/main.c
+++ lab/lab_dev/main.c
@@ -6,39 +6,44 @@
 #define WIDTH (600)
 #define HEIGHT (600)
 #define DELAY (1000 * 5)
+#define X_POS (0)
+#define Y_POS (0)
+#define HEADER ("Pavlovsky")
 
 main(int argc, const char *argv[]) {
 
     /* Initializing variables */
     struct SDL_Window *fortochka = NULL;
     struct SDL_Surface *screenSurface = NULL;
-    struct SDL_Surface *picture = IMG_Load("../pictures/vegetables.png");
+    struct SDL_Surface *picture = NULL;
     assert(picture != NULL);
     struct SDL_Rect dest;
-    dest.x = 0;
-    dest.y = 0;
+    dest.x = X_POS;
+    dest.y = Y_POS;
 
     /* SDL2 */
     if (SDL_Init(SDL_INIT_VIDEO) == -1) {
         printf("Error! %s\n", SDL_GetError());
     } else {
-        fortochka = SDL_CreateWindow("Pavlovsky", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+        fortochka = SDL_CreateWindow(HEADER, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
         if (fortochka == NULL) {
             printf("Error #2! %s\n", SDL_GetError());
         } else {
             screenSurface = SDL_GetWindowSurface(fortochka); /* Getting surface */
             SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xA9, 0xD1, 0x8E));
 
-            SDL_BlitSurface(picture, NULL, screenSurface, &dest);
-            SDL_SetColorKey(picture, SDL_TRUE, SDL_MapRGB(picture->format, 0xFF, 0xFF, 0xFF));
+            /* if ((picture = SDL_LoadBMP("../pictures/vegetables.bmp"))) */
+            if ((picture = IMG_Load("../pictures/vegetables.png"))) {
+                SDL_BlitSurface(picture, NULL, screenSurface, &dest);
+                SDL_SetColorKey(picture, SDL_TRUE, SDL_MapRGB(picture->format, 0xFF, 0xFF, 0xFF));
+            }
 
-            SDL_Delay(200);
+			SDL_Delay(200);
             SDL_UpdateWindowSurface(fortochka);
             SDL_Delay(DELAY);
+
+            SDL_DestroyWindow(fortochka);
         }
+        SDL_Quit();
     }
-
-    SDL_DestroyWindow(fortochka);
-
-    SDL_Quit();
 }
