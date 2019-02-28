b0ad6498895dedb9ea82f6dfba079e216cf56b60
anton2920 <anton2920@gmail.com>
2019-02-28 20:21:47 +0300

    PC: lab_20_01 (dev)

--- lab/lab_dev/main.c
+++ lab/lab_dev/main.c
@@ -11,45 +11,20 @@ struct point {
 };
 
 int SDL_Init_All(struct SDL_Window **, struct SDL_Renderer **);
+void sub_1(struct SDL_Renderer **renderer);
 
 int main(int argc, const char *argv[]) {
 
     /* Initializing variables */
-    int i, j, k;
     struct SDL_Window *window = NULL;
     struct SDL_Renderer *renderer = NULL;
-    struct point pt1 = { 300, 400 };
-    struct point pt2 = {};
 
     /* SDL2 */
     if (!(SDL_Init_All(&window, &renderer))) {
         printf("Error! Code: %s\n", SDL_GetError());
     } else {
 
-        SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0);
-        SDL_RenderClear(renderer);
-
-        SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0x00);
-        SDL_RenderDrawLine(renderer, pt1.x, pt1.y, pt1.x + 200, pt1.y);
-        SDL_RenderDrawLine(renderer, pt1.x, pt1.y, pt1.x + 50, pt1.y - 100);
-        SDL_RenderDrawLine(renderer, pt1.x + 50, pt1.y - 100, pt1.x + 250, pt1.y - 100);
-        SDL_RenderDrawLine(renderer, pt1.x + 200, pt1.y, pt1.x + 250, pt1.y - 100);
-
-        SDL_SetRenderDrawColor(renderer, 0x00, 0xFF, 0x00, 0x00);
-        SDL_RenderDrawLine(renderer, pt1.x, pt1.y, pt2.x, pt2.y);
-
-        SDL_SetRenderDrawColor(renderer, 0x00, 0xFF, 0x00, 0x00);
-        SDL_RenderDrawLine(renderer, pt1.x + 50, pt1.y - 100, pt2.x, pt2.y);
-
-        SDL_SetRenderDrawColor(renderer, 0x00, 0xFF, 0x00, 0x00);
-        SDL_RenderDrawLine(renderer, pt1.x + 200, pt1.y, pt2.x, pt2.y);
-
-        SDL_SetRenderDrawColor(renderer, 0x00, 0xFF, 0x00, 0x00);
-        SDL_RenderDrawLine(renderer, pt1.x + 250, pt1.y - 100, pt2.x, pt2.y);
-
-
-        SDL_Delay(500);
-        SDL_RenderPresent(renderer);
+        sub_1(&renderer);
 
         SDL_Delay(DELAY);
         SDL_DestroyRenderer(renderer);
@@ -66,7 +41,7 @@ int SDL_Init_All(struct SDL_Window **window, struct SDL_Renderer **renderer) {
 
     /* SDL2 */
     SDL_Init(SDL_INIT_VIDEO);
-    if ((*window = SDL_CreateWindow("lab_21", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN)) < 0) {
+    if ((*window = SDL_CreateWindow("lab_20", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN)) < 0) {
         return 0;
     } else if (!(*renderer = SDL_CreateRenderer(*window, -1, 0))) {
         return 0;
@@ -74,4 +49,36 @@ int SDL_Init_All(struct SDL_Window **window, struct SDL_Renderer **renderer) {
 
     /* Returning value */
     return 1;
+}
+
+void sub_1(struct SDL_Renderer **renderer) {
+
+    /* Initializing variables */
+    struct point pt1 = { 300, 400 };
+    struct point pt2 = { 420, 125 };
+
+    SDL_SetRenderDrawColor(*renderer, 0xFF, 0xFF, 0xFF, 0);
+    SDL_RenderClear(*renderer);
+
+    SDL_SetRenderDrawColor(*renderer, 0x01, 0x01, 0x01, 0x00);
+    SDL_RenderDrawLine(*renderer, pt1.x, pt1.y, pt1.x + 200, pt1.y);
+    SDL_RenderDrawLine(*renderer, pt1.x, pt1.y, pt1.x + 60, pt1.y - 100);
+    SDL_RenderDrawLine(*renderer, pt1.x + 60, pt1.y - 100, pt1.x + 260, pt1.y - 100);
+    SDL_RenderDrawLine(*renderer, pt1.x + 200, pt1.y, pt1.x + 260, pt1.y - 100);
+
+    SDL_SetRenderDrawColor(*renderer, 0x00, 0x00, 0xFF, 0x00);
+    SDL_RenderDrawLine(*renderer, pt1.x, pt1.y, pt2.x, pt2.y);
+
+    SDL_SetRenderDrawColor(*renderer, 0x00, 0xFF, 0x00, 0x00);
+    SDL_RenderDrawLine(*renderer, pt1.x + 60, pt1.y - 100, pt2.x, pt2.y);
+
+    SDL_SetRenderDrawColor(*renderer, 0xFF, 0x55, 0x00, 0x00);
+    SDL_RenderDrawLine(*renderer, pt1.x + 200, pt1.y, pt2.x, pt2.y);
+
+    SDL_SetRenderDrawColor(*renderer, 0xFF, 0x00, 0x00, 0x00);
+    SDL_RenderDrawLine(*renderer, pt1.x + 260, pt1.y - 100, pt2.x, pt2.y);
+
+
+    SDL_Delay(500);
+    SDL_RenderPresent(*renderer);
 }
