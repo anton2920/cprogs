4a64f1ffaa837f62f9d603b3ed9671262e9d9bcf
anton2920 <anton2920@gmail.com>
2020-05-29 22:20:48 +0300

    PC: (OS): RGR (done)

---
+++ lab/OS/RGR/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "libs/libs.h"
+
+main() {
+
+    /* Initializing variables */
+    srand(time(NULL));
+
+    /* I/O flow */
+    multiple_queues();
+}
