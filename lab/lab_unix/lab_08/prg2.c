58b3cd512d34546f6b36910661b90054d3dfc8f0
anton <anton2920@gmail.com>
2021-05-19 12:46:06 +0300

    DELL: (Unix): lab_08 (done)

--- lab/lab_unix/lab_08/prg2.c
+++ lab/lab_unix/lab_08/prg2.c
@@ -1,12 +1,66 @@
-#include <stdio.h>
-#include <pthread.h>
 #include <assert.h>
+#include <pthread.h>
+#include <spawn.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+#define handle_error(_msg)  \
+    do {                    \
+        perror(_msg);       \
+        exit(EXIT_FAILURE); \
+    } while (0);
+
+#define error_msg(_msg, ...)                \
+    do {                                    \
+        fprintf(stderr, _msg, __VA_ARGS__); \
+        exit(EXIT_FAILURE);                 \
+    } while (0);
+
+#define TIMEBUF_SIZE 1024
+
+void print_time(const char *supplement)
+{
+    char timebuf[TIMEBUF_SIZE] = {};
+    struct tm *lt;
+    time_t t;
+
+    assert(supplement != NULL);
+
+    t = time(NULL);
+    lt = localtime(&t);
+    strftime(timebuf, TIMEBUF_SIZE, "%T", lt);
+
+    printf("%s:\t\t%s\n", supplement, timebuf);
+}
+
+void print_info(const char *name)
+{
+    printf("Thread name:\t%s\n"
+           "TID:\t\t\t%lu\n",
+           name, pthread_self());
+}
+
+void print_dead(void)
+{
+    printf("Thread is dead!\n");
+}
+
+void print_pid(void)
+{
+    printf("PID:\t\t\t%d\n", getpid());
+}
 
 void *thread_func(void *name)
 {
     assert(name != NULL);
 
-    printf("Name of the thread: %s\n", (char *) name);
+    print_time("Start time");
+    print_info(name);
+    print_pid();
+    print_time("End time");
+    print_dead();
 
     pthread_exit(0);
 }
@@ -14,10 +68,29 @@ void *thread_func(void *name)
 main()
 {
     pthread_t ch1, ch2;
+    pid_t child_pid;
 
-    pthread_create(&ch1, NULL, thread_func, "Thread1");
-    pthread_create(&ch2, NULL, thread_func, "Thread2");
+    child_pid = fork();
 
-    pthread_join(ch1, 0);
-    pthread_join(ch2, 0);
+    if (!child_pid) {
+        system("pidin");
+
+        print_pid();
+        print_time("End time");
+        print_dead();
+    } else {
+        if (pthread_create(&ch1, NULL, thread_func, "Thread1")) {
+            handle_error("pthread_create()");
+        }
+        if (pthread_create(&ch2, NULL, thread_func, "Thread2")) {
+            handle_error("pthread_create()");
+        }
+
+        pthread_join(ch1, 0);
+        pthread_join(ch2, 0);
+
+        print_pid();
+        print_time("End time");
+        print_dead();
+    }
 }
